2ba0144c0945edaa3569c44eb5f240566dceae41
IvanSemukhin <ivansemukhin@gmail.com>
2018-10-30 11:01:08 +0500

    linked list no adt

--- list/main.c
+++ list/main.c
@@ -3,39 +3,60 @@
 #include <string.h>
 
 #define N_SIZE 45
-#define P_MAX 2
 #define BASE_TEN 10
 
 struct person{
     char name[N_SIZE];
     int age;
+    struct person * next;
 };
 
 char * s_gets(char* str, int n);
 
 int main(void)
 {
-    struct person info[P_MAX];
     char convert[N_SIZE];
+    char input[N_SIZE];
     char * tmpCh;
-    int i = 0;
-    int j;
+    struct person * head = NULL;
+    struct person * previous = NULL;
+    struct person * current = NULL;
 
     puts("Enter first name or empty string to exit");
-    while (i < P_MAX && s_gets(info[i].name, N_SIZE) && info[i].name[0] != '\0'){
+    while (s_gets(input, N_SIZE) && input[0] != '\0'){
+        current = (struct person*)malloc(sizeof(struct person));
+
+        if (!head)
+            head = current;
+        else
+            previous->next = current;
+
+        current->next = NULL;
+        strcpy(current->name, input);
         puts("Enter age");
         fgets(convert, N_SIZE, stdin);
-        info[i++].age = strtol(convert, &tmpCh, BASE_TEN);
+        current->age = strtol(convert, &tmpCh, BASE_TEN);
+
         puts("Enter next name or empty string to exit");
+        previous = current;
     }
 
-    if (i == 0)
+    if (!head)
         puts("No data");
     else
         puts("List of data:");
 
-    for (j=0; j<i; j++)
-        printf("Name: [%s] Age: [%d]\n", info[j].name, info[j].age);
+    current = head;
+    while (current){
+        printf("Name:[%s] age[%d]\n", current->name, current->age);
+        current = current->next;
+    }
+
+    while (head){
+        current = head;
+        head = current->next;
+        free(current);
+    }
 
     puts("Program end.");
     return 0;
